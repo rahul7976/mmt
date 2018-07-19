1366d2238564c6c51825d15276e7ab623a999a51
Charles Huang <charles.huang@nasa.gov>
2018-07-19 16:14:43 -0400

    Revert "MMT-873 fixed rubocop warnings in middleware test"
    This reverts commit 6976f50ced940adf0f4d1860d52e30f4f699409b.

--- app/middleware/middleware_healthcheck.rb
+++ app/middleware/middleware_healthcheck.rb
@@ -1,13 +1,12 @@
 class MiddlewareHealthcheck
+  OK_RESPONSE = [ 200, { 'Content-Type' => 'application/json' }, []]
+
   def initialize(app)
     @app = app
   end
 
   def call(env)
     if env['PATH_INFO'.freeze] == '/status'.freeze
-      response = [200, { 'Content-Type' => 'application/json' }]
-
-      # checks the database health
       begin
         db_healthy = ActiveRecord::Migrator.current_version != 0
       rescue StandardError => e
@@ -17,14 +16,15 @@ class MiddlewareHealthcheck
 
       # checks the health of launchpad
       cmr_client = Cmr::Client.client_for_environment(Rails.configuration.cmr_env, Rails.configuration.services)
-      launchpad_healthy = cmr_client.launchpad_healthcheck.body == 'OK'.freeze
-      response[2] = ["{\"database\": #{db_healthy}, \"launchpad\": #{launchpad_healthy}}"]
+      response = cmr_client.launchpad_healthcheck
+      launchpad_healthy = response.body == 'OK'.freeze
+      OK_RESPONSE[2] = ["{\"database\": #{db_healthy}, \"launchpad\": #{launchpad_healthy}}"]
 
       # If launchpad is disabled then we will not report a 500 error if launchpad still fails
-      unless (ENV['launchpad_login_required'] != 'true' || launchpad_healthy) && db_healthy
-        response[0] = 503
+      unless ((ENV['launchpad_login_required'] != 'true' || launchpad_healthy) && db_healthy)
+        OK_RESPONSE[0] = 503;
       end
-      response
+      OK_RESPONSE
     else
       @app.call(env)
     end
