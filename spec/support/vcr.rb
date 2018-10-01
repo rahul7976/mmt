dca8760d4c5c106aa63f17d91d402d0a0cc6b8a8
Charles Huang <charles.huang@nasa.gov>
2018-10-01 11:38:51 -0400

    improve vcr logging. allow http connections when no cassette for testing purposes

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,7 +3,7 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = Rails.root.join('spec', 'vcr')
   c.hook_into :faraday
-  c.allow_http_connections_when_no_cassette = false
+  c.allow_http_connections_when_no_cassette = true
   c.ignore_localhost = true
   c.debug_logger = File.open('./log/vcr.log', 'w')
   c.default_cassette_options = { allow_playback_repeats: true }
@@ -11,7 +11,7 @@ VCR.configure do |c|
     begin
       Rails.logger.debug("Response #{request.method} #{request.uri} result : Headers: #{response.headers} - Body Size (bytes): #{response.body.to_s.bytesize} Status: #{response.status}")
     rescue => e
-      Rails.logger.error "#{self.class} Error: #{e}"
+      Rails.logger.error "#{self.class} Error in VCR after_http_request: #{e}"
     end
   end
 end
